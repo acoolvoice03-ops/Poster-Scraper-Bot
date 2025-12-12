479838fa6258d2c690e869751663b6af3825efad
the-piro <thezake0077@gmail.com>
2025-12-12 15:18:43 +0530

    Update eco.py

--- echobotz/eco.py
+++ echobotz/eco.py
@@ -6,7 +6,7 @@ class echo(object):
 <b>This bot scrapes posters from various OTT platforms and bypasses direct links from cloud sites.</b>
 
 <b>✺ Commands</b>
-<blockquote expandable>
+<blockquote>
 /poster - Scrape any movie/show poster
 /overlap - Overlay a logo on a poster
 /imdb - Search movie/series on IMDb
